0cbf11c7c2fcf1e120a2742f7d8739d8606fb41a
Wallewdev <wallewdev@gmail.com>
2024-07-02 11:39:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  // Display current UTC time
+  function updateTime() {
+    const currentTimeUTC = document.querySelector(
+      '[data-testid="currentTimeUTC"]'
+    );
+    const now = new Date();
+    currentTimeUTC.textContent = now.toUTCString();
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
+
+  // Display current day of the week
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDay = document.querySelector('[data-testid="currentDay"]');
+  const now = new Date();
+  currentDay.textContent = days[now.getUTCDay()];
+};
